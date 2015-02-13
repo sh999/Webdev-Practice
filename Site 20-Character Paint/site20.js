8e3f015ef15fe03c07745ddea9008cf9a9f10444
sh999 <satrio.h@gmail.com>
2015-02-13 18:03:20 -0500

    Added Site 21

--- Site 20-Character Paint/site20.js
+++ Site 20-Character Paint/site20.js
@@ -2,7 +2,7 @@ function writeNumbers() {
 //    document.write("<h1 onclick="change(this)"">Test header</h1>");
     document.write("<h1>nothing special</h1>");
         
-    document.write("<h1>Chaaange</h1>");
+    document.write("<h1 onclick="change(this)">Chaaange</h1>");
     for (var i = 0; i <= 500; i++){
 //        document.write("<p onclick="change(this)"> "+i+" </p>");
         if(i % 100 == 0){
